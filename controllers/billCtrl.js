571b66f690f33213dcc903a65aa1ab0de4cb6b1d
xyadnep <yadab.nepal@wipro.com>
2017-06-16 18:55:51 +0530

    again modified

--- controllers/billCtrl.js
+++ controllers/billCtrl.js
@@ -1,9 +1,15 @@
 app.controller('billCtrl', function ($scope, $http, $timeout, $location, $rootScope, $route,Data) {
 	$scope.order = {custname:'',tableno:'',type:''};
 	
-	$scope.order.items = [{item:'asds',rate:'11',total:'',unit:'asda',quantity:'2'}];
+	$scope.billItems = [{item:'asdf',rate:'',total:'',unit:'',quantity:''}];
 	
 Data.get("getTypes").then(function(results) {
 	$scope.types = results;
 });
+	
+	$scope.addItem= function(item) {
+		$scope.order.items.push(item);
+	}
+	
+	
 });
